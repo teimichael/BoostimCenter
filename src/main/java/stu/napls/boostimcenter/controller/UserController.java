4002e7cc95621e4790152ff368f9f01df0577770
teimichael <teimichael@outlook.com>
2020-02-16 01:24:11 +0900

    Fix the request method of updating avatar.

--- src/main/java/stu/napls/boostimcenter/controller/UserController.java
+++ src/main/java/stu/napls/boostimcenter/controller/UserController.java
@@ -2,22 +2,18 @@ package stu.napls.boostimcenter.controller;
 
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 import stu.napls.boostimcenter.auth.annotation.Auth;
 import stu.napls.boostimcenter.core.exception.Assert;
 import stu.napls.boostimcenter.core.response.Response;
-import stu.napls.boostimcenter.model.Conversation;
 import stu.napls.boostimcenter.model.User;
 import stu.napls.boostimcenter.service.UserService;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 /**
  * @Author Tei Michael
@@ -48,7 +44,7 @@ public class UserController {
                     required = true, dataType = "string", paramType = "header")})
     @Auth
     @PostMapping("/update/avatar")
-    public Response updateAvatar(String avatar, @ApiIgnore HttpSession session) {
+    public Response updateAvatar(@RequestParam String avatar, @ApiIgnore HttpSession session) {
         User user = userService.findUserByUuid(session.getAttribute("uuid").toString());
         Assert.notNull(user, "User does not exist.");
         user.setAvatar(avatar);
